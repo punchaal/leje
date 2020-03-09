4b7828624dc0778d96a53d4573c61dd95bdb5a53
Pratik <punchaal@gmail.com>
2020-03-09 15:32:11 -0400

    added remove functionality for the checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -10,34 +10,35 @@ import {
 
 import './checkout.styles.scss';
 
-const CheckoutPage = ({ cartItems, total }) => (
-  <div className='checkout-page'>
-    <div className='checkout-header'>
-      <div className='header-block'>
-        <span>Product</span>
+const CheckoutPage = ({ cartItems, total }) => {
+  return (
+    <div className='checkout-page'>
+      <div className='checkout-header'>
+        <div className='header-block'>
+          <span>Product</span>
+        </div>
+        <div className='header-blocks'>
+          <span>Description</span>
+        </div>
+        <div className='header-blocks'>
+          <span>Quantity</span>
+        </div>
+        <div className='header-blocks'>
+          <span>Price</span>
+        </div>
+        <div className='header-blocks'>
+          <span>Remove</span>
+        </div>
       </div>
-      <div className='header-blocks'>
-        <span>Description</span>
-      </div>
-      <div className='header-blocks'>
-        <span>Quantity</span>
-      </div>
-      <div className='header-blocks'>
-        <span>Price</span>
-      </div>
-      <div className='header-blocks'>
-        <span>Remove</span>
+      {cartItems.map(cartItem => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <div className='total'>
+        <span>TOTAL : $ {total}</span>
       </div>
     </div>
-    {cartItems.map(cartItem => (
-      <CheckoutItem cartItem={cartItem} key={cartItem.id} />
-    ))}
-    <div className='total'>
-      <span>TOTAL : $ {total}</span>
-    </div>
-  </div>
-);
-
+  );
+};
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
   total: selectCartTotal
